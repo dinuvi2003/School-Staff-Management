2eb4816a8bd1c5655436d525267ac7aa22c58a27
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2025-10-17 10:50:38 +0530

    setup the functionalities related handle the leaves

--- server/src/handlers/repositories/leaveRepositories/leaveRepository.js
+++ server/src/handlers/repositories/leaveRepositories/leaveRepository.js
@@ -9,15 +9,15 @@ export async function repoGetLeaveById(leave_id) {
     return await db.from("leave").select("*").eq("leave_id", leave_id).single();
 }
 
-export async function repoGetLeavesByTeacher(teacher_nic) {
-    return await db.from("leave").select("*").eq("teacher_nic", teacher_nic);
+export async function repoGetLeavesByTeacher(teacher_id) {
+    return await db.from("leave").select("*").eq("teacher_id", teacher_id);
 }
 
-export async function repoGetLeavesByTeacherAndStatus(teacher_nic, status) {
+export async function repoGetLeavesByTeacherAndStatus(teacher_id, status) {
     return await db
         .from("leave")
         .select("*")
-        .eq("teacher_nic", teacher_nic)
+        .eq("teacher_id", teacher_id)
         .eq("leave_status", status);
 }
 
@@ -27,6 +27,24 @@ export async function repoUpdateLeaveStatus(leave_id, newStatus) {
         .from("leave")
         .update({ leave_status: newStatus, leave_updated_date: new Date().toISOString() })
         .eq("leave_id", leave_id)
+        .select("*")
+        .single();
+}
+
+
+// create new leave
+export async function repoCreateNewLeave(teacher_id, leave_type, leave_date, arrival_date, days_count) {
+    return await db
+        .from("leave")
+        .insert([
+            {
+                "teacher_id": teacher_id,
+                "leave_type": leave_type,
+                "leave_date": leave_date,
+                "arrival_date": arrival_date,
+                "leave_day_count": days_count
+            }
+        ])
         .select("*")
         .single();
 }
